8433d688169e106af0e8166d09e37c323fcbf579
Sam Gabrail <sam.gabrail@tekanaid.com>
2024-02-20 19:07:37 -0500

    access grafana

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -71,15 +71,15 @@ resource "aws_security_group" "configu" {
   }
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = 3000
+    to_port     = 3000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port   = 50000
-    to_port     = 50000
+    from_port   = 9090
+    to_port     = 9090
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
